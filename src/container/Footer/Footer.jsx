4a138bb8c6b6c542321d632ba4ffe1f337a3ef2f
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-08-06 17:40:41 +0100

    Special Menu Section completed, fully responsive for mobile, tablet, laptop and desktop screens

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import './Footer.css';
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
-
-export default Footer;
+export default Footer
